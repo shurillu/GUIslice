70556b372eedcff495437ce26e108e452f39ba56
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-03 13:53:41 +0000

    Deploy ImpulseAdventure/GUIslice to github.com/ImpulseAdventure/GUIslice.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,45 @@
+var NAVTREE =
+[
+  [ "GUIslice", "index.html", [
+    [ "GUIslice library", "index.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"GUIslice_8c.html",
+"GUIslice__drv__m5stack_8h.html#a1302e252eef8c80542aa9157521555ef",
+"XGauge_8c.html#a87c28dbdb5f67d826b6ccf372e487295",
+"XTextbox_8c.html#a957b0323390d2ab69c36f6d6c0301e99",
+"group____GeneralFunc__.html#ga3704ed15813869e846d748100194ad81",
+"group____IntElem__.html#ga7f0f458673508775059ae77d21258735",
+"group____Int__.html#ga83b02ba59e5cb2a24007af0f814848b1",
+"group____Int__.html#gaed9d8c85fd8e182ae9e559fb6d1cda68a616ebd50ccb7fb1a1659a9e44723d205",
+"structgslc__tsXRingGauge.html#a091036d038d60278240090010956f887"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
